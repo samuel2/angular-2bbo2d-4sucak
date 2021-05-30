41d4726f95c05382c81965bd45fd416e7cb3472c
samuel2 <smlfolong@gmail.com>
2021-05-30 11:56:38 +0100

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-cart',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  product: Product | undefined;
   constructor() {}
 
   ngOnInit() {}
